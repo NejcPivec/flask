60e17c2da89e9872945a11f79f0f022e99b9c9b7
Nejc Pivec <nejc.pivec1994@gmail.com>
2019-03-08 18:39:10 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/projekti')
+def projekti():
+    return render_template('projekti.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
